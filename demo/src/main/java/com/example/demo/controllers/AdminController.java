cc2469e7b92170baab9ac50762c140864f13c295
Maciej Pietras <maciej.pietras123@gmail.com>
2025-10-13 17:53:30 +0200

    AdminController imports

--- demo/src/main/java/com/example/demo/controllers/AdminController.java
+++ demo/src/main/java/com/example/demo/controllers/AdminController.java
@@ -1,14 +1,28 @@
 package com.example.demo.controllers;
 
-import com.example.demo.entities.User;
-import com.example.demo.entities.enums.Status;
-import com.example.demo.services.*;
-import io.jsonwebtoken.Jwt;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.time.LocalDate;
 import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.ReportedComment;
+import com.example.demo.entities.ReportedMeal;
+import com.example.demo.entities.User;
+import com.example.demo.services.CommentService;
+import com.example.demo.services.FoodService;
+import com.example.demo.services.JwtService;
+import com.example.demo.services.MealService;
+import com.example.demo.services.ReportedCommentService;
+import com.example.demo.services.ReportedMealService;
+import com.example.demo.services.UserService;
 
 @RestController
 @RequestMapping("/admin")
@@ -55,11 +69,9 @@ public class AdminController {
         userService.updateStreak(userId, streak);
         return ResponseEntity.ok(user);
     }*/
-
     //admin
     @PutMapping("/user/{userId}/info/expirationDate")
-    public ResponseEntity<User> updateExpirationDate(int userId, @RequestParam LocalDate date)
-    {
+    public ResponseEntity<User> updateExpirationDate(int userId, @RequestParam LocalDate date) {
         User user = userService.getUserById(userId);
         userService.updatePremiumExpiration(user.getId(), date);
         return ResponseEntity.ok(user);
@@ -68,16 +80,14 @@ public class AdminController {
     //admin
     @ResponseBody
     @GetMapping("/users")
-    public ResponseEntity<List<User>> getAllUsers()
-    {
+    public ResponseEntity<List<User>> getAllUsers() {
         List<User> allUsers = userService.getAllUsers();
         return ResponseEntity.ok(allUsers);
     }
 
     //admin
     @GetMapping("reports/meals/user/{userId}")
-    public ResponseEntity<List<ReportedMeal>> getAllMealReportsByUser(@PathVariable int userId)
-    {
+    public ResponseEntity<List<ReportedMeal>> getAllMealReportsByUser(@PathVariable int userId) {
         User user = userService.getUserById(userId);
         List<ReportedMeal> reports = reportedMealService.getAllReportsByUser(userId);
         return ResponseEntity.ok(reports);
@@ -85,8 +95,7 @@ public class AdminController {
 
     //admin
     @GetMapping("reports/comments/user/{userId}")
-    public ResponseEntity<List<ReportedComment>> getAllCommentReportsByUser(@PathVariable int userId)
-    {
+    public ResponseEntity<List<ReportedComment>> getAllCommentReportsByUser(@PathVariable int userId) {
         User user = userService.getUserById(userId);
         List<ReportedComment> reports = reportedCommentService.getAllReportsByUser(userId);
 
