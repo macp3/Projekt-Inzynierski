38bb2792b8f9f7bbd636de9e7fcbb148aacf6d62
BartoszPilka <bartoszpilka02@gmail.com>
2025-10-16 01:37:43 +0200

    Testing phase - AdminController briefly verified. Changed compose.yml

--- snackTrack/src/main/java/study/snacktrack/controllers/AdminController.java
+++ snackTrack/src/main/java/study/snacktrack/controllers/AdminController.java
@@ -52,38 +52,31 @@ public class AdminController {
         this.notificationService = notificationService;
     }
 
+    //dziala
     @GetMapping("/dashboard")
     public String getDashboard() {
         return "Witaj, ADMIN!";
     }
 
     //nowe
+    //dziala
     @GetMapping("/user/{userId}")
     public ResponseEntity<User> getUserInfo(@PathVariable int userId) {
         User user = userService.getUserById(userId);
         return ResponseEntity.ok(user);
     }
 
-    //doi naprawy - updatowanie streaka: albo +1 albo wyzerowac
-    /*@GetMapping("/{userId}/updateStreak")
-    public ResponseEntity<User>updateStreak(@PathVariable int userId, @RequestParam int streak)
-    {
-
-        User user = userService.getUserById(userId);
-
-        user.setStreak(streak);
-        userService.updateStreak(userId, streak);
-        return ResponseEntity.ok(user);
-    }*/
     //admin
+    //pozniej przetestujemy bo trzeba zrestartowac
     @PutMapping("/user/{userId}/info/expirationDate")
-    public ResponseEntity<User> updateExpirationDate(int userId, @RequestParam LocalDate date) {
+    public ResponseEntity<User> updateExpirationDate(@PathVariable int userId, @RequestParam LocalDate date) {
         User user = userService.getUserById(userId);
         userService.updatePremiumExpiration(user.getId(), date);
         return ResponseEntity.ok(user);
     }
 
     //admin
+    //dziala
     @ResponseBody
     @GetMapping("/users")
     public ResponseEntity<List<User>> getAllUsers() {
@@ -92,14 +85,16 @@ public class AdminController {
     }
 
     //admin
-    @GetMapping("reports/meals/user/{userId}")
+    //przetestujemy pozniej bo odjebalem
+    @GetMapping("/reports/meals/user/{userId}")
     public ResponseEntity<List<ReportedMeal>> getAllMealReportsByUser(@PathVariable int userId) {
         List<ReportedMeal> reports = reportedMealService.getAllReportsByUser(userId);
         return ResponseEntity.ok(reports);
     }
 
     //admin
-    @GetMapping("reports/comments/user/{userId}")
+    //pozniej bo trzeba zrobic report i tu tez odpierdolilem
+    @GetMapping("/reports/comments/user/{userId}")
     public ResponseEntity<List<ReportedComment>> getAllCommentReportsByUser(@PathVariable int userId) {
         List<ReportedComment> reports = reportedCommentService.getAllReportsByUser(userId);
 
@@ -108,15 +103,20 @@ public class AdminController {
 
     ////////////////////////////////////////////////////////////////////////
 
+    //dziala
     @GetMapping("/trainings")
     public ResponseEntity<List<TrainingInfo>> getAllTrainings() {
         return ResponseEntity.ok(trainingService.getAllTrainings());
     }
 
+    //
     @GetMapping("/trainings/{trainingId}/details")
     public ResponseEntity<TrainingDetailsResponse> getTrainingDetails(@PathVariable int trainingId) {
         return ResponseEntity.ok(trainingService.getTrainingDetails(trainingId));
     }
+
+    //dodac walidacje duration time
+    //walidacja exeercise id musi byc z bazy
 
     @PostMapping("/trainings/add")
     public ResponseEntity<String> addTraining(@RequestBody TrainingRequest request) {
@@ -124,8 +124,7 @@ public class AdminController {
         return ResponseEntity.ok("Training added successfully");
     }
 
-    //pojebane - nie moga byc dwa te same cwiczenia tego samego dnia
-
+    //dodac walidacje exercise id musi byc w bazie
     @PutMapping("/trainings/{trainingId}/edit")
     public ResponseEntity<String> editTraining(@PathVariable int trainingId, @RequestBody TrainingRequest request, @RequestHeader("Authorization") String authHeader)
     {
@@ -139,6 +138,8 @@ public class AdminController {
     }
 
     //dodawanie cwiczenia do treningu
+    //nie wykrywa trainingid a w bazie on istnieje
+    //potem przetestuj wszystko
     @PostMapping("/trainings/addExercise")
     public ResponseEntity<TrainingDetailsResponse> addExerciseToTraining(@RequestBody AddExerciseToTrainingRequest request, @RequestHeader("Authorization") String authHeader) {
         //walidacja admina
@@ -155,18 +156,22 @@ public class AdminController {
         return ResponseEntity.ok(response);
     }
 
+    //nie znajduje training o id 1 a jest on w bazie
+    //potem przetestuj wszystko
     @DeleteMapping("/trainings/{trainingId}/delete/{exerciseId}")
     public ResponseEntity<TrainingDetailsResponse> deleteExercisesByIdFromTraining(@PathVariable int trainingId, @PathVariable int exerciseId)
     {
         return ResponseEntity.ok(trainingService.deleteAllExercisesByIdFromTraining(trainingId, exerciseId));
     }
 
+    //dziala
     @DeleteMapping("/trainings/{trainingId}/delete/{exerciseId}/{dayOfExercise}")
     public ResponseEntity<TrainingDetailsResponse> deleteExerciseByIdAndDayFromTraining(@PathVariable int trainingId, @PathVariable int exerciseId, @PathVariable int dayOfExercise)
     {
         return ResponseEntity.ok(trainingService.deleteExerciseByIdAndDayFromTraining(trainingId, exerciseId, dayOfExercise));
     }
 
+    //dziala
     @DeleteMapping("/trainings/{trainingId}/delete")
     public ResponseEntity<String> deleteTraining(@PathVariable int trainingId)
     {
@@ -174,22 +179,26 @@ public class AdminController {
         return ResponseEntity.ok("Training deleted successfully");
     }
 
+    //dziala
     @GetMapping("/exercises")
     public ResponseEntity<List<Exercise>> getAllExercises() {
         return ResponseEntity.ok(trainingService.getAllExercises());
     }
 
+    //dziala
     @GetMapping("/exercises/{exerciseId}/details")
     public ResponseEntity<Exercise> getExerciseDetails(@PathVariable int exerciseId) {
         return ResponseEntity.ok(trainingService.getExerciseById(exerciseId));
     }
 
+    //wszystkie pola z intami maja byc przyrownane do null i bedzie dzialac
     @PostMapping("/exercises/add")
     public ResponseEntity<Exercise> addExercise(@RequestBody ExerciseRequest request) {
         Exercise exercise = trainingService.createExercise(request.getName(), request.getDescription(), request.getType(), request.getDifficulty(), request.getNumberOfSets(), request.getRepetitionsPerSet());
         return ResponseEntity.ok(exercise);
     }
 
+    //dziala
     @DeleteMapping("/exercises/delete/{exerciseId}")
     public ResponseEntity<String> deleteExercise(@PathVariable int exerciseId) {
         trainingService.deleteExercise(exerciseId);
@@ -199,6 +208,9 @@ public class AdminController {
 
     ////////////////////////////////////////////////////////////////////
 
+    //wyjebac login, email, password xd
+    //walidacja stringow - nic nie wyrzuca exeption
+    //porownanie do nulli tak samo
     @PostMapping("/notifications/add")
     public ResponseEntity<Notification> createNotification(
             @RequestBody NotificationRequest request,
@@ -213,6 +225,7 @@ public class AdminController {
         return ResponseEntity.ok(notification);
     }
 
+    //pola nie moga byc nullami
     @GetMapping("/notifications")
     public ResponseEntity<List<Notification>> getAllNotifications(
             @RequestHeader("Authorization") String authHeader
@@ -226,6 +239,7 @@ public class AdminController {
         return ResponseEntity.ok(notifications);
     }
 
+    //recipients=gowno musi cos wyrzucac
     @GetMapping("/notifications/filter")
     public ResponseEntity<List<Notification>> getNotificationsByRecipients(
             @RequestHeader("Authorization") String authHeader,
