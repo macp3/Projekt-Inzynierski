de0eec1f0cd4ffb0011dbdfeb267b3d61036db1a
BartoszPilka <bartoszpilka02@gmail.com>
2025-11-11 08:12:46 +0100

    Changed database structure and added some functionalities

---
+++ snackTrack/src/main/java/study/snacktrack/dto/EssentialFoodRequest.java
@@ -0,0 +1,99 @@
+package study.snacktrack.dto;
+
+public class EssentialFoodRequest
+{
+    private String name;
+    private String description;
+    private float calories;
+    private float protein;
+    private float fat;
+    private float carbohydrates;
+    private String brandName;
+    private float defaultWeight;
+    private String servingSizeUnit;
+
+
+    public EssentialFoodRequest(String name, String description, float calories, float protein, float fat, float carbohydrates, String brandName, float defaultWeight, String servingSizeUnit) {
+        this.name = name;
+        this.description = description;
+        this.calories = calories;
+        this.protein = protein;
+        this.fat = fat;
+        this.carbohydrates = carbohydrates;
+        this.brandName = brandName;
+        this.defaultWeight = defaultWeight;
+        this.servingSizeUnit = servingSizeUnit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getCalories() {
+        return calories;
+    }
+
+    public void setCalories(float calories) {
+        this.calories = calories;
+    }
+
+    public float getProtein() {
+        return protein;
+    }
+
+    public void setProtein(float protein) {
+        this.protein = protein;
+    }
+
+    public float getFat() {
+        return fat;
+    }
+
+    public void setFat(float fat) {
+        this.fat = fat;
+    }
+
+    public float getCarbohydrates() {
+        return carbohydrates;
+    }
+
+    public void setCarbohydrates(float carbohydrates) {
+        this.carbohydrates = carbohydrates;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public float getDefaultWeight() {
+        return defaultWeight;
+    }
+
+    public void setDefaultWeight(float defaultWeight) {
+        this.defaultWeight = defaultWeight;
+    }
+
+    public String getServingSizeUnit() {
+        return servingSizeUnit;
+    }
+
+    public void setServingSizeUnit(String servingSizeUnit) {
+        this.servingSizeUnit = servingSizeUnit;
+    }
+}
