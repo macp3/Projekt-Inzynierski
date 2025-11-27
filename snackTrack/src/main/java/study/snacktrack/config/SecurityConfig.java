c23870880bd4f15a8405d67bd8a6b3fe9f6cb428
macp3 <s99663@pollub.edu.pl>
2025-11-27 22:45:38 +0100

    testing CORS change

--- snackTrack/src/main/java/study/snacktrack/config/SecurityConfig.java
+++ snackTrack/src/main/java/study/snacktrack/config/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", frontendUrl));
+        configuration.addAllowedOriginPattern("*");
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
